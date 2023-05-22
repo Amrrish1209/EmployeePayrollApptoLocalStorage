6b04821276d9ce05b47f4b7f542f707ae9700d9f
Amrrish1209 <roshanbabubl@gmail.com>
2023-05-22 10:14:58 +0530

    Add: Ability to save the Employee Payroll Object to Local Storage.

--- js/employeePayrollData.js
+++ js/employeePayrollData.js
@@ -1,6 +1,7 @@
 class EmployeePayrollData {
+  //getter and setter method
   get id() {
-    return (this._id = id);
+    return this._name;
   }
   set id(id) {
     this._id = id;
@@ -9,8 +10,8 @@ class EmployeePayrollData {
     return this._name;
   }
   set name(name) {
-    let nameRegex = RegExp("^[A-Z]{1}[a-zA-z\\s]{2,}$");
-    if (nameRegex.test(name)) this._name = name;
+    let nameREgex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+    if (nameREgex.test(name)) this._name = name;
     else throw "Name is Incorrect";
   }
   get profilePic() {
@@ -19,76 +20,129 @@ class EmployeePayrollData {
   set profilePic(profilePic) {
     this._profilePic = profilePic;
   }
+
   get gender() {
     return this._gender;
   }
   set gender(gender) {
     this._gender = gender;
   }
+
   get department() {
-    return this._deparment;
+    return this._department;
   }
   set department(department) {
-    this._deparment = department;
+    this._department = department;
   }
+
   get salary() {
     return this._salary;
   }
   set salary(salary) {
     this._salary = salary;
   }
+
   get note() {
     return this._note;
   }
   set note(note) {
     this._note = note;
   }
-  get startDate() {
+
+  get satrtDate() {
     return this._startDate;
   }
-  set startDate(startDate) {
-    let currentDate = new Date();
-    if (startDate > currentDate) {
-      throw "start date is a future date";
-    }
-    var diff = Math.abs(currentDate.getTime - startDate.getTime());
-    if (diff / (1000 * 60 * 60 * 24) > 30) {
-      throw "start date is beyond 30 days";
-    }
-    this._startDate = startDate;
+  set satrtDate(satrtDate) {
+    this._startDate = satrtDate;
   }
+  //method
   toString() {
     const options = { year: "numeric", month: "long", day: "numeric" };
-    const empDate = !this._startDate
+    const empDate = !this.satrtDate
       ? "undefined"
-      : this.startDate.toLocalDateString("en-US", options);
+      : this.satrtDate.toLocaleDateString("en-US", options);
+
     return (
       "id=" +
       this.id +
-      ",name='" +
+      " ,name = " +
       this.name +
-      ", gender='" +
+      ",gender=" +
       this.gender +
-      ",profilePic='" +
+      ",profilePic=" +
       this.profilePic +
-      ", department=" +
+      ",department=" +
       this.department +
-      ", salary=" +
+      ",salary=" +
       this.salary +
-      ", startDate=" +
+      ",startDate = " +
       empDate +
-      ", note=" +
+      ",note=" +
       this.note
     );
   }
 }
+
+/**
+ * Ability to set Event Listeners when Document is loaded so as to.
+ */
+//..........UC2...........
+window.addEventListener("DOMContentLoaded", (event) => {
+  const name = document.querySelector("#name");
+  const textError = document.querySelector(".text-error");
+  name.addEventListener("input", function () {
+    if (name.value.length == 0) {
+      textError.textContent = "";
+      return;
+    }
+    try {
+      new EmployeePayrollData().name = name.value;
+      textError.textContent = "";
+    } catch (e) {
+      textError.textContent = e;
+    }
+  });
+
+  const salary = document.querySelector("#salary");
+  const output = document.querySelector(".salary-output");
+  output.textContent = salary.value;
+  salary.addEventListener("input", function () {
+    output.textContent = salary.value;
+  });
+});
+
+//..........UC3 AND UC4
+/**
+ *
+ * Ability to save the Employee Payroll Object to Local Storage.
+ * @returns it returns the data in EmployeepayrollData.
+ */
+
 const save = () => {
   try {
     let employeePayrollData = createEmployeePayroll();
+    createAndUpdateStorage(employeePayrollData);
   } catch (e) {
     return;
   }
 };
+
+function createAndUpdateStorage(employeePayrollData) {
+  let employeePayrollList = JSON.parse(
+    localStorage.getItem("EmployeePayrollList")
+  );
+
+  if (employeePayrollList != undefined) {
+    employeePayrollList.push(employeePayrollData);
+  } else {
+    employeePayrollList = [employeePayrollData];
+  }
+  alert(employeePayrollList.toString());
+  localStorage.setItem(
+    "EmployeePayrollList",
+    JSON.stringify(employeePayrollList)
+  );
+}
 
 const createEmployeePayroll = () => {
   let employeePayrollData = new EmployeePayrollData();
@@ -96,12 +150,15 @@ const createEmployeePayroll = () => {
     employeePayrollData.name = getInputValueById("#name");
   } catch (e) {
     setTextValue(".text-error", e);
+    throw e;
   }
+
   employeePayrollData.profilePic = getSelectedValues("[name=profile]").pop();
   employeePayrollData.gender = getSelectedValues("[name=gender]").pop();
   employeePayrollData.department = getSelectedValues("[name=department]");
   employeePayrollData.salary = getInputValueById("#salary");
   employeePayrollData.note = getInputValueById("#notes");
+
   let date =
     getInputValueById("#day") +
     " " +
@@ -109,17 +166,15 @@ const createEmployeePayroll = () => {
     " " +
     getInputValueById("#year");
   employeePayrollData.date = Date.parse(date);
-  alert(employeePayrollData.toString());
   return employeePayrollData;
 };
-
 const getSelectedValues = (propertyValue) => {
   let allItems = document.querySelectorAll(propertyValue);
-  let selItems = [];
+  let setItems = [];
   allItems.forEach((item) => {
-    if (item.checked) selItems.push(item.value);
+    if (item.checked) setItems.push(item.value);
   });
-  return selItems;
+  return setItems;
 };
 
 const getInputValueById = (id) => {
